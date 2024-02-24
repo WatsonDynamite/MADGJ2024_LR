cd4aa9d34faf36e6a9013d93ffcdef7ac4b2654c
Watson <jrld98@gmail.com>
2024-02-24 18:09:15 +0000

    added several UI elements

--- Scripts/Protag.gd
+++ Scripts/Protag.gd
@@ -1,12 +1,29 @@
 extends CharacterBody3D
 
+const MAX_AMMO = 10;
+
+const AMMO_SPRITE_WIDTH = 16;
+const AMMO_SPRITE_HEIGHT = 65;
+const HP_SPRITE_WIDTH = 77;
+const HP_SPRITE_HEIGHT = 65;
+
 @export var speed = 4
 var model: Node3D 
 var bullet: PackedScene  = preload("res://Prefabs/bullet.tscn")
+
+@onready var ammoMeter: TextureRect = $Control/AmmoMeter;
+@onready var hpMeter: TextureRect = $Control/HpMeter;
+
+@onready var reloadingLabel: Label3D = $ReloadingLabel
+@onready var outOfAmmoLabel: Label3D = $OutOfAmmoLabel
+
+@onready var reloadTimer: Timer = $ReloadTimer
+
 var gunDirection: Node3D
 var level: Node3D
 var ammo: Array
-var ammoValue = 10
+
+var ammoValue = MAX_AMMO
 var currentBullet = 0
 var health = 3
 
@@ -27,6 +44,7 @@ func get_input(delta):
 	model.rotation = Vector3(model.rotation.x,lerp_angle(model.rotation.y, angle, delta * 100), model.rotation.z)
 	
 	if Input.is_action_just_pressed("shoot") && ammoValue > 0:
+		
 		ammo[currentBullet].set_process_mode(PROCESS_MODE_ALWAYS)
 		ammo[currentBullet].show()
 		ammo[currentBullet].position = gunDirection.global_position
@@ -35,27 +53,41 @@ func get_input(delta):
 		ammoValue -= 1
 		if currentBullet == 15:
 			currentBullet = 0
+	if(ammoValue == 0):
+		outOfAmmoLabel.visible = true;
 	if Input.is_action_just_pressed("reload"):
-		ammoValue = 10
+		outOfAmmoLabel.visible = false;
+		reloadingLabel.visible = true;
+		reloadTimer.start();
+	ammoMeter.set_size(Vector2(AMMO_SPRITE_WIDTH * ammoValue, AMMO_SPRITE_HEIGHT));
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	model = get_node("Model")
 	gunDirection = get_node("Model/GunDirection")
 	level = get_parent()
 	ammo = level.get_node("Bullets").get_children()
+	hpMeter.set_size(Vector2(HP_SPRITE_WIDTH * health, HP_SPRITE_HEIGHT))
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-
 func _physics_process(delta):
 	get_input(delta)
 	move_and_slide()
+	pass
 	
 func takeDamage(value):
 	health -= value
-	print(health)
+	hpMeter.set_size(Vector2(HP_SPRITE_WIDTH * health, HP_SPRITE_HEIGHT));
+	print(health);
+	pass
 	
+func _on_reload_timer_timeout():
+	outOfAmmoLabel.visible = false;
+	reloadingLabel.visible = false;
+	ammoValue = 10;
+	ammoMeter.set_size(Vector2(AMMO_SPRITE_WIDTH * ammoValue, AMMO_SPRITE_HEIGHT));
+	pass # Replace with function body.
