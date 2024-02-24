8cdbab97faa4318b48d0326b5286cc5ae6ba4f04
António Rocha <afgrocha98@gmail.com>
2024-02-24 15:05:48 +0000

    Level enemy spawner

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -16,6 +16,19 @@ func setDirection(vector):
 	transform = vector
 	
 func _on_area_entered(area):
-	hide()
+	print("teste")
+	_disable()
 	area.queue_free()
 	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	print("parede")
+	body.get_parent()._decress_curr_size(2)
+	_disable()
+	pass # Replace with function body.
+
+func _disable():
+	hide()
+	set_process_mode(PROCESS_MODE_DISABLED)
+	position.y = 10
