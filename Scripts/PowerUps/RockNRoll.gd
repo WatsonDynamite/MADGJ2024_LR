516a2d8a8115ebbe4df5da344eefaca7bdc1d647
António Rocha <afgrocha98@gmail.com>
2024-02-25 07:58:32 +0000

    Rock n roll power up polish

--- Scripts/PowerUps/RockNRoll.gd
+++ Scripts/PowerUps/RockNRoll.gd
@@ -1,6 +1,6 @@
 extends Area3D
 
-
+@onready var audio = $AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,7 +11,8 @@ func _process(delta):
 	pass
 
 func _powerUp():
+	audio.play()
 	var children: Array = get_parent().get_node("Enemies").get_children()
 	for i in children.size():
-		children[i].queue_free()
+		children[i]._on_receive_damage(10)
 	
